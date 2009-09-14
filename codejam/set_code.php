dcf923e54d9a921dceb7abb515b1fd954348d1bb
Sandaruwan Gunathilake <sandaruwan@gunathilake.com>
2009-09-14 14:36:14 +0530

    update submission languages

--- codejam/set_code.php
+++ codejam/set_code.php
@@ -8,14 +8,33 @@
     $users[$row['handle']] = $row['id'];
   }
   
-  $submissions = file('submissions',FILE_IGNORE_NEW_LINES);
-  $i = 0;
-  foreach($submissions as $submission) {
-    $data = split("\|",$submission);
-    $q = "UPDATE score SET score.lang='".$data[5]."' WHERE score.user_id=".$users[$data[0]]." AND competition_id=1 AND question=".($data[3]-1);
-    mysql_query($q, $conn);
-    $i++;
-    if (($i%1000)==0)
-      echo $i."\n";
+  function update($submissions,$round) {
+    global $conn;
+    global $users;
+    echo "Round -- $round\n";
+    $i = 0;
+    foreach($submissions as $submission) {
+      $data = split("\|",$submission);
+      $q = "UPDATE score SET score.lang='".$data[5]."' WHERE score.user_id=".$users[$data[0]]." AND competition_id=2 AND question=".($data[3]-1);
+      mysql_query($q, $conn);
+      $i++;
+      if (($i%1000)==0)
+        echo $i."\n";
+    }
   }
+  
+  $r1a = file('round1a',FILE_IGNORE_NEW_LINES);
+  $r1b = file('round1b',FILE_IGNORE_NEW_LINES);
+  $r1c = file('round1c',FILE_IGNORE_NEW_LINES);
+  update($r1a,2);
+  update($r1b,3);
+  update($r1c,4);
+  
+  /*
+    SQL UPDATES - too lazy to add this into code now, later
+    UPDATE score SET lang='CPP' WHERE lang='C++';
+    UPDATE score SET lang='CSharp' WHERE lang='C#';
+    UPDATE score SET lang='Unknown' WHERE lang='?';
+    UPDATE score SET lang='FSharp' WHERE lang='F#';    
+  */
 ?>
